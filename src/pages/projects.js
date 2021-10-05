a0dd7e7f0c5489ffd426989797def3eb775fd376
Carlos <carlosperalta88@gmail.com>
2021-10-05 00:06:54 -0300

    fixes

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,28 +4,22 @@ import Layout from '../components/layout'
 const Projects = ({location}) => {
   return (
     <Layout location={location} title="Peralta Media">
-      <div>
-        <div>
-          <article>
-            <header>
-              <h1>
-                I Killed The Romance
-              </h1>
-            </header>
-            <section>
-              <p>
-                Sound escapes and electronic influences. I've been working on and off this project in the last
-                five years. I'm starting to publish again. The challenge is to produce a track every week and publish it.
-              </p>
-              <a 
-                href="https://soundcloud.com/i-killed-the-romance"
-                target="_blank"
-                rel="noreferrer"
-                >SoundCloud</a>
-            </section>
-          </article>
-        </div>
-      </div>
+      <article>
+        <header>
+          <h2>
+            I Killed The Romance
+          </h2>
+        </header>
+          <p>
+            Sound escapes and electronic influences. I've been working on and off this project in the last
+            five years. I'm starting to publish again. The challenge is to produce a track every week and publish it.
+          </p>
+          <a 
+            href="https://soundcloud.com/i-killed-the-romance"
+            target="_blank"
+            rel="noreferrer"
+            >SoundCloud</a>
+      </article>
     </Layout>
   )
 }
